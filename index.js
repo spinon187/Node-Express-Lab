76296058521922a9714cbc9e012dbbc535f2b033
spinon187 <spinon187@gmail.com>
2019-02-14 17:43:51 -0600

    heroku

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const server = require('./server');
 
-server.listen(5000, () => {
-    console.log('\n main screen turn on \n');
+const port = process.env.PORT || 5000;
+
+server.listen(port, () => {
+    console.log(`\n listening on ${port} \n`);
 });
